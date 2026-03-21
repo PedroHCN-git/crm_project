b28fda73648164da0fed2bf2a4ed0957633995b5
PedroHCN-git <pedro.hcorradini05@gmail.com>
2026-03-21 14:57:02 -0300

    alterações arquiteturais e testes implementados

--- app/tests/fake_user_repository.py
+++ app/tests/fake_user_repository.py
@@ -1,44 +1,37 @@
 from app.repositories.user_repository_interface import UserRepositoryInterface
 from app.exceptions.repository import NotFoundError
 from app.entities.user import User
-from app.infra.user_orm import UserORM
-from sqlalchemy import Engine
-from sqlalchemy.orm import Session
-from sqlalchemy import select
 
 
 class FakeUserRepository(UserRepositoryInterface):
-    def __init__(self, engine: Engine):
-        self.engine = engine
-        self.session = Session(engine)
+    def __init__(self):
+        self.users: dict[int, User] = {}
+        self.id_count: int = 1
 
     def save(self, user: User):
-        try:
-            with self.session as s:
-                user_orm = s.query(UserORM).filter_by(id=user.id).first()
+        # trocar banco por dicionario users
 
-                if user_orm:
-                    user_orm.name = user.name
-                    user_orm.email = user.email
-                    user_orm.password = user.password
-                    user_orm.blocked = user.blocked
-                else:
-                    new_user = UserORM(
-                        name=user.name,
-                        email=user.email,
-                        password=user.password,
-                        blocked=user.blocked
-                    )
-                    s.add(new_user)
-                
-                s.commit()
-        except Exception as e:
-            s.rollback()
+        _id = user.id
 
-            raise e
-    
+        if _id == None:
+            user.__id = _id
+            _id = self.id_count
 
-    def get_by_id_or_fail(self, id: int):
+        
+        _user = self.users.get(user.id)
+
+        if _user:
+            _user.name = user.name
+            _user.email = user.email
+            _user.password = user.password
+            _user.blocked = user.blocked
+        else:
+            _user = user
+
+        self.users.update({_user.id: _user})
+
+
+    def get_by_id_or_fail(self, id: int) -> User:
 
         user = self.get_by_id(id)
         if not user:
@@ -46,47 +39,13 @@ class FakeUserRepository(UserRepositoryInterface):
 
         return user
     
-    def get_by_id(self, id):
-        stmt = select(UserORM).where(UserORM.id == id)
-        results = self.session.scalars(stmt)
+    def get_by_id(self, id) -> User | None:
 
-        user = None
-        user_data = None
-
-        for result in results:
-            user_data = result
-
-        if user_data:
-            user = User(
-                user_data.name,
-                user_data.email,
-                user_data.password,
-                user_data.blocked,
-                user_data.id
-            )
+        user = self.users.get(id)
 
         return user
         
 
-    def list(self):
-        stmt = select(UserORM)
-        results = self.session.scalars(stmt)
+    def list(self) -> list[User]:
 
-        user_list = []
-        user_data = None
-
-        for result in results:
-            user_data = result
-
-            if user_data:
-                user = User(
-                    user_data.name,
-                    user_data.email,
-                    user_data.password,
-                    user_data.blocked,
-                    user_data.id
-                )
-
-                user_list.append(user)
-
-        return user_list
+        return [user for user in self.users.values()]
